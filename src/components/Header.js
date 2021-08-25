a1e097481a9bd6e413ba268c338cc24151014739
harshvardhan jha <harshjha038@gmail.com>
2021-08-25 15:25:15 +0530

    header aligned

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
    const showSidebar = () => setSidebar(!sidebar);
 
   return (
-   <Container className=" mt-5 mb-3">
+   <Container fluid className=" mt-5 mb-4">
       <Row>
          <Col className="Nav">
             <nav className="Nav">
@@ -24,13 +24,13 @@ function Header() {
                   </svg>
                </Link>
             <div className={sidebar ? 'navi active' : 'navi'}>  
-               <div className="navi " onClick={showSidebar}>
+               <div className="navi" onClick={showSidebar}>
                   <Link className="navigation" to="#">Philosophy</Link>
                   <Link className="navigation" to="#">Perspectives</Link>
                   <Link className="navigation" to="#">People</Link>
                   <Link className="navigation" to="#">Blog</Link>
                   <Link className="navigation" to="mailto:info@arcadeagroup.com">
-                     Contact
+                     Contact 
                      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M4.66669 11.3333L11.3334 4.66663" stroke="#221F1F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
                         <path d="M4.66669 4.66663H11.3334V11.3333" stroke="#221F1F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
