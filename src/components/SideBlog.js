2bb8b38083dd7ba4694ac3184884c1fbb217d165
harshvardhan jha <harshjha038@gmail.com>
2021-08-26 19:04:44 +0530

    comment section removed from sideblog and header aligned

--- src/components/SideBlog.js
+++ src/components/SideBlog.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 import * as moment from 'moment';
 import { useParams } from "react-router-dom";
 import categoryService from './../services/categoryService';
@@ -50,7 +51,7 @@ function SideBlog() {
 
   }, [slug]);
 
-  const location = useLocation();
+  // const location = useLocation();
 
   return (
     <>
@@ -60,9 +61,9 @@ function SideBlog() {
               <div className="nav nav-tabs" id="nav-tab" role="tablist">
                 <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Popular</button>                
                 <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Recents</button>
-                {(location.pathname === "/blog-detail/"+slug)?
+                {/* {(location.pathname === "/blog-detail/"+slug)?
                 (<button className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Comments</button>)
-                : ('')}
+                : ('')} */}
               </div>
           </nav>
            <div className="tab-content" id="nav-tabContent"><div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
